233ce037d6cde5d82f8d7b5c514cd9a1437b8e51
Peter Haro <peter.haro@sintef.no>
2015-08-11 09:16:19 +0200

    Added existing functionality to the API through Retrofit annotations

--- app/src/main/java/fiskinfoo/no/sintef/fiskinfoo/Http/BarentswatchApiRetrofit/IBarentswatchApi.java
+++ app/src/main/java/fiskinfoo/no/sintef/fiskinfoo/Http/BarentswatchApiRetrofit/IBarentswatchApi.java
@@ -1,7 +1,25 @@
 package fiskinfoo.no.sintef.fiskinfoo.Http.BarentswatchApiRetrofit;
 
+import java.util.List;
+
+import fiskinfoo.no.sintef.fiskinfoo.Http.BarentswatchApiRetrofit.models.PropertyDescription;
+import fiskinfoo.no.sintef.fiskinfoo.Http.BarentswatchApiRetrofit.models.Subscription;
+import retrofit.Callback;
+import retrofit.http.GET;
+
 public interface IBarentswatchApi {
-    //@GET()
-    //@Post()
-    //YOLOSWAGGERMCDAGGER
+    String subscribable = "/service/subscribable/";
+    String geoDataSubscription = "/subscription/";
+    String geoDataDownload = "/download/{ApiName}?format={Format}";
+
+    @GET(subscribable)
+    List<PropertyDescription> getSubscribable(Callback<List<PropertyDescription>> callback);
+
+    @GET(geoDataSubscription)
+    List<Subscription> getSubscriptions(Callback<List<Subscription>> callback);
+
+    //@Get(geoDataDownload)
+    // TODO: Add bw:file object and such
+    //File<T> geoDataDownload(@Path("ApiName")String apiName, @Path("Format") String format
+
 }
